23955bd5361a5e439e108f82840592e7673f68b8
re1yun <re1yun@knu.ac.kr>
2023-10-23 09:14:05 +0900

    added mongo loader and connection to mongo db

--- src/loaders/index.js
+++ src/loaders/index.js
@@ -1,9 +1,12 @@
 import expressLoader from './express.js';
+import mongoLoader from './mongo.js';
 import router from '../api/index.js';
 
 export default async ( app ) => {
     await expressLoader(app);
     console.log('Express Initialized');
-
+    await mongoLoader(app);
+    console.log('Mongo Initialized');
+    
     app.use('/', router);
 }
---
+++ src/loaders/mongo.js
@@ -0,0 +1,10 @@
+// src/loaders/mongo.js
+import * as mongoose from 'mongoose';
+
+export default async (app) => {
+    const connection = await mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    return connection.connection.db;
+}
